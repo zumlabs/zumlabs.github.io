f999b6e114716b18c47c9102b96451de50ea1e30
zumlabs <qoriakbar28@gmail.com>
2023-02-26 01:59:05 +0700

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -85,12 +85,7 @@ const config = {
       from: '2022',
       to: '2025',
     },
-    {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2012',
-      to: '2014',
-    },
+    
   ],
 
   // To hide the `My Projects` section, keep it empty.
